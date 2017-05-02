e05ecf02cc01e49aa70a8ea76036421e5f41a1ba
trhallam <trhallam@gmail.com>
2017-05-02 14:31:58 +1000

    Added -p option to main for sys.path import .
    Can now run trib when sys.path is incomplete by adding -p to autoupdate
    sys.path before executing program.

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ Antony Hallam
 
 import sys, os, inspect
 
+if '-p' in sys.argv:
+	trib_folder = os.path.realpath(
+					os.path.abspath(
+						os.path.dirname(
+							inspect.getfile( inspect.currentframe() ))))
+	if trib_folder not in sys.path:
+		sys.path.insert(0,trib_folder)
+		
+	subpaths = ['lib', 'tribgui', os.path.join('tribgui','_qtdesigner')]
+	iter = 1
+	for sub in subpaths:
+		tribsubpath = os.path.join(trib_folder,sub)
+		if tribsubpath not in sys.path:
+			sys.path.insert(iter, tribsubpath)
+			iter+=1
+
 # connect to lib packages
 libpath = os.path.abspath(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())),'lib'))
 # add libpath to sys.path
