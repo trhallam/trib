5b8509edcc687bd66dbc62bfe59f13ef6127f8e7
trhallam <trhallam@gmail.com>
2017-05-02 16:38:59 +1000

    Reconfigure path and add options doc

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-"""trib_main.py
+"""main.py
 
 Main function for trib application
 
 Antony Hallam
 2017-04-25
 
+Has sys.argv options
+	
+	-p		Adds subfolders of trib directory to sys.path if missing.
+
 """
 
 
 import sys, os, inspect
 
+trib_folder = os.path.realpath(os.path.abspath(os.path.dirname(
+						inspect.getfile( inspect.currentframe() ))))
+
+# sort out missing pythong path variables with -p option
 if '-p' in sys.argv:
-	trib_folder = os.path.realpath(
-					os.path.abspath(
-						os.path.dirname(
-							inspect.getfile( inspect.currentframe() ))))
 	if trib_folder not in sys.path:
 		sys.path.insert(0,trib_folder)
 		
